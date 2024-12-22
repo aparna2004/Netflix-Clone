c38bfa62bea00c95458de6b0487ee57b2903f563
Aparna Sathyamurthi Thambu <aparna.thambu@gmail.com>
2024-12-22 15:34:05 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,10 @@
 ob_start(); // Turns on output buffering.
 session_start(); // We are able to use sessions.
 
-date_default_timezone_set("Europe/Budapest"); // Setting the time zone.
+date_default_timezone_set("Asia/Kolkata"); // Setting the time zone to India.
 
 try {
-    $con = new PDO("mysql:dbname=reeceflix;host=localhost", "admin", "admin");
+    $con = new PDO("mysql:dbname=netflix-clone;host=localhost", "admin", "admin");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
     exit("Connection failed: " . $e->getMessage());
